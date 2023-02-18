78898b6a65f9cddcc082a606b2e7e390cf399dd8
MayureshDMadav <mayuresh.madav@getsimpl.com>
2023-02-18 18:55:54 +0530

    old

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+var request = require("request");
+const app = express();
+
+app.get("/backend", (req, res) => {
+  let data = req.query;
+
+  console.log(JSON.stringify(data));
+
+  var options = {
+    method: "POST",
+    url: "https://script.google.com/macros/s/AKfycby3FGhp1Y7iX7Mn4X6EPWOisEjZzdhgLR1mD_d6tQLGme9hbiEYUoBc0GiVHBBhJAgq/exec?action=addData",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  async function post(options) {
+    await request(options, function (error, response) {
+      if (error) throw new Error(error);
+      console.log(response.body);
+    });
+  }
+
+  post(options);
+
+  res.send("Successfully Added");
+});
+
+app.listen(5000, console.log("server started"));
